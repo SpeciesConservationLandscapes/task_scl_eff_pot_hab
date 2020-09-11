789a806dc4e24aa34a9d404dd034a18052960af5
Nathaniel Robinson <nrobinson@panthera.org>
2020-09-11 09:43:25 -0600

    Use dynamic tiger density table
    1. Use available density estimates for ecoregions. if none available use average for biome. if none available for biome, use a default

--- src/task_scl_eff_pot_hab.py
+++ src/task_scl_eff_pot_hab.py
@@ -3,7 +3,6 @@ import ee
 from datetime import datetime, timezone
 from task_base import SCLTask
 
-
 class SCLPolygons(SCLTask):
     ee_rootdir = "projects/SCL/v1"
     scale = 300
@@ -21,7 +20,7 @@ class SCLPolygons(SCLTask):
         "probability": {
             "ee_type": SCLTask.IMAGECOLLECTION,
             "ee_path": "projects/SCL/v1/Panthera_tigris/geographies/Sumatra/hab/probability",
-            "maxage": 1, 
+            "maxage": 1,
         },
         "historic_range": {
             "ee_type": SCLTask.IMAGE,
@@ -48,9 +47,14 @@ class SCLPolygons(SCLTask):
             "ee_path": "projects/SCL/v1/Panthera_tigris/source/Inputs_2006/eco_cntry",
             "static": True
         },
+        "ecoregions": {
+            "ee_type": SCLTask.FEATURECOLLECTION,
+            "ee_path": "RESOLVE/ECOREGIONS/2017",
+            "static": True
+        },
         "density": {
-            "ee_type": SCLTask.IMAGE,
-            "ee_path": "projects/SCL/v1/Panthera_tigris/source/Inputs_2006/density",
+            "ee_type": SCLTask.FEATURECOLLECTION,
+            "ee_path": "projects/SCL/v1/Panthera_tigris/source/density/tiger_obs_density",
             "static": True
         },
     }
@@ -60,11 +64,6 @@ class SCLPolygons(SCLTask):
         "patch_size": 5,
         "hii": 6,
         "probability": 1,
-    }
-    remap_lists = {
-        "density": [10, 20, 30, 40, 50, 60, 70, 80, 90],
-        "min_patch_size": [250, 250, 625, 250, 140, 70, 45, 30, 250],
-        "min_stepping_stone_size": [25, 25, 63, 25, 14, 7, 5, 3, 25],
     }
 
     def __init__(self, *args, **kwargs):
@@ -82,7 +81,8 @@ class SCLPolygons(SCLTask):
         self.extirpated = ee.Image(self.inputs["extirpated"]["ee_path"])
         self.elevation = ee.Image(self.inputs["elevation"]["ee_path"])
         self.ecoregion_country = ee.Image(self.inputs["ecoregion_country"]["ee_path"])
-        self.density = ee.Image(self.inputs["density"]["ee_path"])
+        self.ecoregions = ee.FeatureCollection(self.inputs["ecoregions"]["ee_path"])
+        self.density = ee.FeatureCollection(self.inputs["density"]["ee_path"])
         self.water = ee.Image(self.inputs["water"]["ee_path"])
         self.set_aoi_from_ee(
             "{}/{}/sumatra_poc_aoi".format(self.ee_rootdir, self.species)
@@ -131,12 +131,33 @@ class SCLPolygons(SCLTask):
 
         hii_ecoregion = self.ecoregion_country.add(hii_sum)
 
-        min_patch_size = self.density.remap(
-            self.remap_lists["density"], self.remap_lists["min_patch_size"]
-        ).updateMask(hii_ecoregion)
-        min_stepping_stone_size = self.density.remap(
-            self.remap_lists["density"], self.remap_lists["min_stepping_stone_size"]
-        ).updateMask(hii_ecoregion)
+        def density_to_patch_size(ft):
+            med_density_eco = ee.Number(ft.get('MED_DENSITY_ECO'))
+            med_density_biome = ee.Number(ft.get('MED_DENSITY_ECO'))
+            med_density_eco_biome = ee.Number(ft.get('MED_DENSITY_ECO'))
+            density_val = ee.Algorithms.If(med_density_eco.gt(0),
+                med_density_eco,
+                ee.Algorithms.If(med_density_biome.gt(0),
+                    med_density_biome,
+                    ee.Algorithms.If(med_density_eco_biome.gt(0),
+                        med_density_eco_biome,
+                        1)))
+            min_core_size = ee.Number(500).divide(density_val).int()
+            return ee.Feature(None, {'ECO_ID': ft.get('ECO_ID'), 'min_core_size': min_core_size})
+
+        density = self.density.map(density_to_patch_size)
+
+        ecoregion_image = self.ecoregions.reduceToImage(
+            properties=['ECO_ID'],
+            reducer=ee.Reducer.mode()
+        )
+
+        ecoregion_id = density.aggregate_array('ECO_ID')
+        core_size = density.aggregate_array('min_core_size')
+
+        min_patch_size = ecoregion_image.remap(
+            ecoregion_id, core_size).int().clamp(30, 625).updateMask(hii_ecoregion)
+        min_stepping_stone_size = min_patch_size.divide(10).int().updateMask(hii_ecoregion)
 
         hii_grp = hii_ecoregion.addBands(
             [
@@ -181,7 +202,7 @@ class SCLPolygons(SCLTask):
             ],
         )
 
-        scl_eff_pot_hab = "{}/geographies/Sumatra/scl_poly/{}/scl_eff_pot_hab".format(
+        scl_eff_pot_hab = "{}/geographies/Sumatra/scl_poly/{}/scl_eff_pot_hab_test".format(
             self.species, self.taskdate
         )
 
